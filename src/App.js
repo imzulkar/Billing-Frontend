bf16b89cb4225a8b15454738bf18adc3e159a0f3
Gm Zulkar Nine <gmzulkar@gmail.com>
2022-10-20 16:46:42 +0600

    initial boiler plate ready with login system

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import Signin from './components/authentication/login';
+import Profile from './components/profile';
 
 function App() {
+  const token = localStorage.getItem('accessToken');
+  const path = window.location.pathname
+  if(!token) {
+    return <Signin />
+  }
+  if(token && path ==='/'){
+    // console.log(window.history);
+    return window.history.back()
+  }
+  const Test1 = () => {
+    return <>
+    <p>Test 1 </p>
+    </>
+  }
+  const Test2 = () => {
+    return <>
+    <p>Test 2 </p>
+    </>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Signin/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/t1' element={<Test1/>}/>
+          <Route path='/t2' element={<Test2/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
